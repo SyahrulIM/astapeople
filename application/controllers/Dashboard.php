7beb1bff44f90653ea58740bd76ab527cda9dd15
Syahrul Izha <74164767+SyahrulIM@users.noreply.github.com>
2025-12-10 10:56:21 +0700

    update dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -14,45 +14,8 @@ class Dashboard extends CI_Controller
 
     public function index()
     {
-        $title = 'Dashboard';
-
-        // Start Acc Detail Shopee
-        $this->db->select('
-            acc_shopee_detail.no_faktur,
-            MAX(acc_shopee_detail.pay_date) AS pay_date,
-            MAX(acc_shopee_detail.total_faktur) AS total_faktur,
-            MAX(acc_shopee_detail.pay) AS pay,
-            MAX(acc_shopee_detail.discount) AS discount,
-            MAX(acc_shopee_detail.payment) AS payment,
-            MAX(acc_shopee_detail.order_date) AS order_date
-        ');
-        $this->db->from('acc_shopee_detail');
-        $this->db->join('acc_shopee', 'acc_shopee.idacc_shopee = acc_shopee_detail.idacc_shopee');
-        $this->db->join('user', 'user.iduser = acc_shopee.iduser');
-        $this->db->group_by('acc_shopee_detail.no_faktur');
-        $acc_shopee_detail = $this->db->get();
-        // End
-
-        // Start Acc Detail Accurate
-        $this->db->select('
-            acc_accurate_detail.no_faktur,
-            MAX(acc_accurate_detail.pay_date) AS pay_date,
-            MAX(acc_accurate_detail.total_faktur) AS total_faktur,
-            MAX(acc_accurate_detail.pay) AS pay,
-            MAX(acc_accurate_detail.discount) AS discount,
-            MAX(acc_accurate_detail.payment) AS payment
-        ');
-        $this->db->from('acc_accurate_detail');
-        $this->db->join('acc_accurate', 'acc_accurate.idacc_accurate = acc_accurate_detail.idacc_accurate');
-        $this->db->join('user', 'user.iduser = acc_accurate.iduser');
-        $this->db->group_by('acc_accurate_detail.no_faktur');
-        $acc_accurate_detail = $this->db->get();
-        // End
-
         $data = [
-            'title' => $title,
-            'acc_shopee_detail' => $acc_shopee_detail->result(),
-            'acc_accurate_detail' => $acc_accurate_detail->result()
+            'title' => 'Dashboard',
         ];
 
         $this->load->view('theme/v_head', $data);
--- application/views/Dashboard/v_dashboard.php
+++ application/views/Dashboard/v_dashboard.php
@@ -1,26 +1,16 @@
-            <!-- Page content-->
-            <div class="container-fluid">
-                <div class="row mt-4">
-                    <div class="col text-center">
-                        <h1>Welcome Asta People</h1>
-                        <h3>Better life Better home</h3>
-                    </div>
-                </div>
-            </div>
-            </div>
-            </div>
-            <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-            <!-- 2. DataTables JS -->
-            <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-            <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
-            <script src="https://cdn.datatables.net/rowreorder/1.5.0/js/dataTables.rowReorder.js"></script>
-            <script src="https://cdn.datatables.net/rowreorder/1.5.0/js/rowReorder.dataTables.js"></script>
-            <script src="https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js"></script>
-            <script src="https://cdn.datatables.net/responsive/3.0.4/js/responsive.dataTables.js"></script>
-            <!-- 3. Bootstrap bundle -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-            <!-- 4. Core theme JS -->
-            <script src="<?php echo base_url(); ?>js/scripts.js"></script>
-            </body>
+<!-- Page content-->
+<div class="container-fluid">
+    <div class="row mt-5">
+        <div class="col text-center">
+            <h1>Welcome Asta People</h1>
+            <h3>Better life Better home</h3>
+        </div>
+    </div>
+</div>
 
-            </html>
+<!-- Hanya load script yang dibutuhkan -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('js/scripts.js'); ?>"></script>
+</body>
+
+</html>
